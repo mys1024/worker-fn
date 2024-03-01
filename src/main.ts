dd7a98f81def38f3129d9ac4e6b8e59d65a41037
mys1024 <mys.1024@outlook.com>
2024-03-01 21:44:10 +0800

    docs: add module docs

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
+/**
+ * `worker-fn` hides the complexity of communication between the main thread and Worker threads,
+ * making it easy to call functions defined in the Worker.
+ * @module
+ */
+
 export { defineWorkerFn, defineWorkerFns } from "./worker.ts";
 export { inspectWorker, useWorkerFn, useWorkerFns } from "./proxy.ts";
